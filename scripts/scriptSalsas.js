0027fc281867ceb73854e35c9bc768a654fd4242
Juan Pablo Solís Rodriguez <juanpablo.solisrodriguez@gmail.com>
2024-12-09 15:43:39 -0300

    implementación búsqueda salsas

--- scripts/scriptSalsas.js
+++ scripts/scriptSalsas.js
@@ -33,6 +33,46 @@ function cierreModal(){
     }
 }
 
+function buscar(){
+   
+
+    var texto = document.getElementById("txtBuscar").value;
+    alert("btnBuscar " +texto);
+
+    
+    
+    
+   
+
+    const tabla = document.querySelector('#tabla tbody')
+    tabla.innerHTML = '';
+
+
+
+    // fetch('https://localhost/LyaSushi_Api_v2/Ingrediente/api/Ingrediente/Get')
+    fetch('https://apirestaurant.somee.com/api/Salsa/Buscar/' +texto)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data)
+
+           
+
+            for (let i = 0; i < data.response.length; i++) {
+                const fila = document.createElement('tr');
+                fila.innerHTML += `
+        <td data-cell="idIngrediente">${data.response[i].idIngrediente}</td>
+        <td data-cell="descripcion">${data.response[i].descripcion}</td>        
+        <td data-cell="editar"><a href="#" id="btnEditar" onclick="modalEditar('${data.response[i].idIngrediente}', '${data.response[i].descripcion}', 'Editar')">  <i class="fa fa-edit"></i></a></td>
+        <td data-cell="eliminar"><a href="#" id="btnEliminar" onclick="modalEditar('${data.response[i].idIngrediente}', '${data.response[i].descripcion}', 'Eliminar')"><i class="fa fa-trash"></i></a></td>
+        `;
+                tabla.appendChild(fila);
+            }
+        });
+
+
+
+}
+
 function ejecutarAccion(accion, idSalsa, descripcionSalsa, precioUnitario, precio, cantidad)
 {
     console.log("Ejecutar accion " +accion+ " "+idSalsa+" "+descripcionSalsa +" "+ precioUnitario +" "+precio+" "+cantidad );
